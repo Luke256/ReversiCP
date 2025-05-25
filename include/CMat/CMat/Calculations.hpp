2d115b1f02428f44e6b35c2729ea197ac69ab6f3
Luke <55425834+Luke256@users.noreply.github.com>
2025-05-25 17:54:42 +0900

    CMat::sumRows

---
+++ include/CMat/CMat/Calculations.hpp
@@ -0,0 +1,26 @@
+﻿# pragma once
+
+# include "Matrix.hpp"
+
+namespace CMat
+{
+	template<class _Dty>
+	Matrix<_Dty> sumRows(const Matrix<_Dty>& x)
+	{
+		Matrix<_Dty>res(MatShape{ 1, x.shape.cols });
+		auto* ptr = x.data();
+		auto* target_ptr;
+
+		uint32_t i, j;
+
+		for (i = 0; i < x.shape.rows; ++i)
+		{
+			target_ptr = res.data();
+			for (j = 0; j < x.shape.cols; ++j)
+			{
+				*target_ptr++ += *ptr++;
+			}
+		}
+		return res;
+	}
+}
