6aca616c417d97dd6a1cce943b7a6cb07b25cd24
Luke256 <yuta.k.200628@gmail.com>
2025-06-17 14:08:03 +0900

    use 3 layer to simplenet

--- include/NNCpp/Modules/SimpleNet.hpp
+++ include/NNCpp/Modules/SimpleNet.hpp
@@ -8,25 +8,29 @@ namespace NNCpp::Modules
 	class SimpleNet
 	{
 	private:
-		Dense<_Dty> d1, d2;
-		HiddenActivation activator;
+		Dense<_Dty> d1, d2, d3;
+		HiddenActivation a1, a2;
 	public:
 		SimpleNet(uint32_t inputSize, uint32_t hiddenSize, uint32_t outputSize):
-			d1(inputSize, hiddenSize), d2(hiddenSize, outputSize),
-			activator()
+			d1(inputSize, hiddenSize), d2(hiddenSize, hiddenSize), d3(hiddenSize, outputSize),
+			a1(), a2()
 		{}
 
 		void forward(const CMat::Matrix<_Dty>& input, CMat::Matrix<_Dty>& output)
 		{
 			d1.forward(input, output);
-			activator.forward(output, output);
+			a1.forward(output, output);
 			d2.forward(output, output);
+			a2.forward(output, output);
+			d3.forward(output, output);
 		}
 
 		void backward(const CMat::Matrix<_Dty>& input, CMat::Matrix<_Dty>& output)
 		{
-			d2.backward(input, output);
-			activator.backward(output, output);
+			d3.backward(input, output);
+			a2.backward(output, output);
+			d2.backward(output, output);
+			a1.backward(output, output);
 			d1.backward(output, output);
 		}
 
@@ -34,7 +38,9 @@ namespace NNCpp::Modules
 		{
 			NNCpp::Utils::ParamInfoList<_Dty>res = d1.parameters();
 			NNCpp::Utils::ParamInfoList<_Dty>p2 = d2.parameters();
+			NNCpp::Utils::ParamInfoList<_Dty>p3 = d3.parameters();
 			res.insert(res.end(), p2.begin(), p2.end());
+			res.insert(res.end(), p3.begin(), p3.end());
 			return res;
 		}
 	};
